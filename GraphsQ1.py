ae5de38f73b9ce62f345e4fff15318fd132529d5
Hao CHEN <hche608@aucklanduni.ac.nz>
2015-10-18 10:47:17 +1300

    Updated Code

--- GraphsQ1.py
+++ GraphsQ1.py
@@ -10,21 +10,17 @@ def main(input):
     G = nx.DiGraph()
     counter = 0
     number_of_nodes = 0
-    lines = input.split('\n')   
+    lines = input.split('\n')  
     for line in lines:
         if counter == number_of_nodes:
-            counter = 0
-            try:
-                number_of_nodes = int(line)
-                if number_of_nodes == 0:
-                    print(nx.number_weakly_connected_components(G))
-                    break
-            except ValueError:
-                break
+            number_of_nodes = int(line)
             if G.number_of_nodes() > 0:
                 print(nx.number_weakly_connected_components(G))
                 G.clear()
-                G.add_nodes_from(range(0,number_of_nodes))                          
+            if number_of_nodes == 0:
+                break    
+            G.add_nodes_from(range(0,number_of_nodes))
+            counter = 0                          
         else:
             nodes = line.split(' ')
             for n in nodes:
--- GraphsQ2.py
+++ GraphsQ2.py
@@ -6,41 +6,94 @@
 import sys
 import networkx as nx
 
-def longest_path(G):
-    subDAGList = nx.topological_sort(G)  
-    count = 0
-            
-    for i in range(subDAGList.index(0),len(nx.topological_sort(G))-1):
-        if nx.topological_sort(G)[i]>nx.topological_sort(G)[i+1]:
-            count-=1
-        count+=1    
-    return count+1
 
-def subDAG(G):
-    for dag in nx.weakly_connected_component_subgraphs(G):
-        if dag.has_node(0):
-            print(longest_path(dag))
-            break
+def dag_longest_path(G, weight='weight', default_weight=1):
+    """Returns the longest path in a DAG
+    If G has edges with 'weight' attribute the edge data are used as weight values.
 
+    Parameters
+    ----------
+    G : NetworkX DiGraph
+        Graph
+
+    weight : string (default 'weight')
+        Edge data key to use for weight
+
+    default_weight : integer (default 1)
+        The weight of edges that do not have a weight attribute
+
+    Returns
+    -------
+    path : list
+        Longest path
+
+    Raises
+    ------
+    NetworkXNotImplemented
+        If G is not directed
+
+    See also
+    --------
+    dag_longest_path_length
+    """
+    dist = {} # stores {v : (length, u)}
+    for v in nx.topological_sort(G):
+        us = [(dist[u][0] + data.get(weight, default_weight), u)
+            for u, data in G.pred[v].items()]
+        # Use the best predecessor if there is one and its distance is non-negative, otherwise terminate.
+        maxu = max(us) if us else (0, v)
+        dist[v] = maxu if maxu[0] >= 0 else (0, v)
+    u = None
+    v = max(dist, key=dist.get)
+    path = []
+    while u != v:
+        path.append(v)
+        u = v
+        v = dist[v][1]
+    path.reverse()
+    return path
+
+def longest(G):
+    #print(G.nodes())
+    pathlist = dag_longest_path(G)
+    try:
+        print(len(pathlist) - pathlist.index(0))
+    except ValueError:
+        for dag in nx.weakly_connected_component_subgraphs(G):
+            if dag.has_node(0):
+                longest(dag)
+                break
+       
+    
+    # pathlist = dag_longest_path(G)
+    # if not 0 in pathlist:
+    #     for dag in nx.weakly_connected_component_subgraphs(G):
+    #         if dag.has_node(0):
+    #             pathlist = dag_longest_path(dag)
+    #             break
+    #
+    # for i in range(0,len(pathlist)):
+    #     if pathlist[i] == 0:
+    #         print(len(pathlist)-i)
+    #         break
+    # #print(len(pathlist) - pathlist.index(0))
+
+               
 def main(input):
     G = nx.DiGraph()
     counter = 0
     number_of_nodes = 0
-    lines = input.split('\n')   
+    lines = input.split('\n')  
     for line in lines:
         if counter == number_of_nodes:
-            counter = 0
-            try:
-                number_of_nodes = int(line)
-                if number_of_nodes == 0:
-                    subDAG(G)
-                    break
-            except ValueError:
-                break
+            number_of_nodes = int(line)
             if G.number_of_nodes() > 0:
-                subDAG(G)
+                longest(G)
                 G.clear()
-                G.add_nodes_from(range(0,number_of_nodes))                              
+            if number_of_nodes == 0:
+                break    
+            G.add_nodes_from(range(0,number_of_nodes))
+            counter = 0                          
         else:
             nodes = line.split(' ')
             for n in nodes:
